fd4dd6b9c53ac2a8f294ef4f0ed0020eb7a848e6
Mayank Tiwari <mk4528513@gmail.com>
2025-02-10 10:54:30 +0530

    Add files via upload

---
+++ 01_variable.js
@@ -0,0 +1,13 @@
+const accountId = 12345
+let accountEmail = "mayank123@gmail.com"
+var password = "1234"
+accouuntCity = "jaipur"
+let accountState;
+accountEmail = "mk213@gmail.com"
+
+// prefer not to use var because of issue in clock ScriptProcessorNode
+// and functional scope
+console.log(accountId);
+console.table([accountEmail,password,accouuntCity,accountId , accountState]);
+
+
---
+++ 04_comparison.js
@@ -0,0 +1,14 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("01" > 1)
+
+// console.log(null > 0);
+// console.log(null == 0); // comparison convert null to a number , treating it as 0.
+// console.log(null >= 0);
+
+// strict check "==="
+console.log("2" === 2);
